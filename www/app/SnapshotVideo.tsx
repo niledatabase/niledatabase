fa2c5718565c07af0882046b09171a0f7e3a07d5
jrea <juzko.r@gmail.com>
2023-07-27 14:34:15 -0400

    chore: design rev

---
+++ www/app/SnapshotVideo.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { useRef, useEffect, useState, RefObject } from "react";
+
+const useIntersection = (element: RefObject<HTMLElement>) => {
+  const [isVisible, setState] = useState(false);
+
+  useEffect(() => {
+    const handleObserver: IntersectionObserverCallback = ([entry]) => {
+      if (entry.isIntersecting) {
+        setState(entry.isIntersecting);
+        if (element.current) {
+          observer.unobserve(element.current);
+        }
+      }
+    };
+    const observer = new IntersectionObserver(handleObserver, {
+      rootMargin: "-200px",
+    });
+
+    element.current && observer.observe(element.current);
+
+    return () => {
+      if (element.current) {
+        observer.unobserve(element.current);
+      }
+    };
+  }, []);
+
+  return isVisible;
+};
+export default function SnapshotVideo() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const isVisible = useIntersection(videoRef);
+
+  useEffect(() => {
+    if (videoRef && videoRef.current && isVisible) {
+      videoRef.current.play();
+    }
+  }, [videoRef, isVisible]);
+
+  return (
+    <video muted ref={videoRef} loop poster="/dashboard.svg">
+      <source src="/snapshot.mp4" />
+    </video>
+  );
+}
