80cffdf1d7d47e86bb6d0df87a8ca3cf6b909393
Scott Delamater <923236+scottydawg@users.noreply.github.com>
2023-09-01 13:17:15 -0400

    feat: redirect to dashboard page on auth success

--- examples/user_management/social_login_google/NextJS/app/api/auth/handler/route.ts
+++ examples/user_management/social_login_google/NextJS/app/api/auth/handler/route.ts
@@ -1,4 +1,6 @@
-import jwtDecode, { JwtPayload } from "jwt-decode";
+import jwtDecode from "jwt-decode";
+import { cookies } from 'next/headers';
+import AuthCookieData, { NileJWTPayload } from "@/app/model/AuthCookieData";
 
 export async function POST(req: Request) {
   const formData = await req.formData();
@@ -9,17 +11,35 @@ export async function POST(req: Request) {
   }
 
   try {
-    const accessToken = formData.get("access_token") as string;
-    const decodedJWT = jwtDecode<JwtPayload>(accessToken);
+    const cookieData = toCookieData(formData);
 
-    return new Response(JSON.stringify({
-      token: accessToken,
-      subject: decodedJWT.sub,
-      audience: decodedJWT.aud,
-      state: formData.get("state"),
-      event,
-    }, null, 2));
+    cookies().set('authData', JSON.stringify(cookieData), {
+      httpOnly: false,
+      secure: process.env.NODE_ENV !== 'development', // Use HTTPS in production
+      maxAge: 3600, // 1 hour
+      path: '/',
+    });
+
+    return new Response(JSON.stringify(cookieData), {
+      headers: {
+        'Location': '/dashboard',
+      },
+      status: 302,
+    });
   } catch (e) {
     return new Response(JSON.stringify(e as Error), {status: 500});
   }
+}
+
+function toCookieData(formData: FormData): AuthCookieData {
+  const accessToken = formData.get("access_token") as string;
+  const decodedJWT = jwtDecode<NileJWTPayload>(accessToken);
+  return {
+    accessToken: formData.get("access_token") as string,
+    state: formData.get("state") as string,
+    event: formData.get("event") as string,
+    error: formData.get("error") as string,
+    tenantId: formData.get("tenantId") as string,
+    tokenData: decodedJWT,
+  };
 }
