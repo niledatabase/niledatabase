f75ea8d54f9c0a5c67727d0b958bd7d0a9de3abe
Scott Delamater <923236+scottydawg@users.noreply.github.com>
2023-09-01 15:57:40 -0400

    feat: handle auth errors gracefully

--- examples/user_management/social_login_google/NextJS/app/api/auth/handler/route.ts
+++ examples/user_management/social_login_google/NextJS/app/api/auth/handler/route.ts
@@ -1,45 +1,64 @@
-import jwtDecode from "jwt-decode";
+import jwtDecode from 'jwt-decode';
 import { cookies } from 'next/headers';
-import AuthCookieData, { NileJWTPayload } from "@/app/model/AuthCookieData";
+import AuthCookieData, { NileJWTPayload } from '@/app/model/AuthCookieData';
+import { ResponseCookie } from 'next/dist/compiled/@edge-runtime/cookies';
 
 export async function POST(req: Request) {
   const formData = await req.formData();
-  const event = formData.get("event");
+  const event = formData.get('event');
 
-  if (event === "AUTH_ERROR") {
-    return new Response(JSON.stringify({ error: formData.get("error") }), { status: 500 });
+  let location: string;
+
+  if (event === 'AUTH_ERROR') {
+    const message = formData.get('error');
+    location = redirectOnError(message ? message.toString() : 'Unknown error');
+  } else {
+    location = redirectOnSuccess(formData);
   }
 
+  return new Response(null, {
+    headers: { 'Location': location },
+    status: 302,
+  });
+}
+
+function redirectOnError(message: string): string {
+  cookies().set('errorData', JSON.stringify({
+    message: message,
+  }), buildCookieOptions(100));
+
+  return '/';
+}
+
+function buildCookieOptions(maxAge: number): Partial<ResponseCookie> {
+  return {
+    httpOnly: false,
+    secure: process.env.NODE_ENV !== 'development', // Use HTTPS in production
+    maxAge: maxAge,
+    path: '/',
+  };
+}
+
+function redirectOnSuccess(formData: FormData): string {
   try {
     const cookieData = toCookieData(formData);
+    cookies().set('authData', JSON.stringify(cookieData), buildCookieOptions(3600));
 
-    cookies().set('authData', JSON.stringify(cookieData), {
-      httpOnly: false,
-      secure: process.env.NODE_ENV !== 'development', // Use HTTPS in production
-      maxAge: 3600, // 1 hour
-      path: '/',
-    });
-
-    return new Response(JSON.stringify(cookieData), {
-      headers: {
-        'Location': '/dashboard',
-      },
-      status: 302,
-    });
+    return '/dashboard';
   } catch (e) {
-    return new Response(JSON.stringify(e as Error), {status: 500});
+    return redirectOnError((e as Error).message);
   }
 }
 
 function toCookieData(formData: FormData): AuthCookieData {
-  const accessToken = formData.get("access_token") as string;
+  const accessToken = formData.get('access_token') as string;
   const decodedJWT = jwtDecode<NileJWTPayload>(accessToken);
   return {
-    accessToken: formData.get("access_token") as string,
-    state: formData.get("state") as string,
-    event: formData.get("event") as string,
-    error: formData.get("error") as string,
-    tenantId: formData.get("tenantId") as string,
+    accessToken: formData.get('access_token') as string,
+    state: formData.get('state') as string,
+    event: formData.get('event') as string,
+    error: formData.get('error') as string,
+    tenantId: formData.get('tenantId') as string,
     tokenData: decodedJWT,
   };
 }
