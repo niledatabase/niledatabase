2a75df253f3b4e4d93e23628037e88aba8b63ec3
sriramsub <sriram.sub@gmail.com>
2023-09-02 18:21:05 -0700

    fix some more issues

--- www/app/docs/_components/SideNavigation/types.ts
+++ www/app/docs/_components/SideNavigation/types.ts
@@ -1,6 +1,6 @@
 // the values need to match the directory name
 // update buildNavParams as well as this
 export enum NavigationRoots {
-  Guides = "guides",
+  GetStarted = "getting-started",
   UserManagement = "user-management",
 }
