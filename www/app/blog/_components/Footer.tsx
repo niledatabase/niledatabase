8c53859d5146db5ad5a9ebc8047628dec35796e9
jrea <juzko.r@gmail.com>
2023-09-05 15:46:49 -0400

    fix: use the right path for templates

---
+++ www/app/blog/_components/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <div>
+      <div className="bg-gradient-text bg-clip-text text-transparent leading-normal text-xl z-10 relative">
+        Get the latest posts to your inbox
+      </div>
+      <div className="opacity-60">
+        We respect your time and your inbox, we will not spam you with unrelated
+        stuff.
+      </div>
+    </div>
+  );
+}
