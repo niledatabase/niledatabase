eff6860187564d06940a582872329924b5b6d3ed
Gwen Shapira <cshapi@gmail.com>
2023-09-19 15:52:47 -0700

    running curl now

--- examples/quickstart/java/src/main/java/com/example/todowebapp/xterm/XtermController.java
+++ examples/quickstart/java/src/main/java/com/example/todowebapp/xterm/XtermController.java
@@ -4,10 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.web.socket.TextMessage;
+import org.jline.reader.impl.DefaultParser;
+import static org.toilelibre.libe.curl.Curl.curl;
+import static org.toilelibre.libe.curl.Curl.$;
+
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Controller
 public class XtermController {
+
+    private DefaultParser lineParser = new DefaultParser();
+
     @GetMapping("/xterm")
     public String xterm() {
         return "xterm";
@@ -17,7 +27,16 @@ public class XtermController {
     @MessageMapping("/xterm")
     @SendTo("/resp")
     public XTermResponse xtermCmd(XTermCommand cmd) {
-        System.out.println("got websocket message!" + cmd.getCommand());
-        return new XTermResponse("Got your message. You said: " + cmd.getCommand() );
+        String strCmd = cmd.getCommand();
+
+        System.out.println("got websocket message!" + strCmd);
+
+        List<String> parsed = lineParser.parse(strCmd, 0).words();
+        if (!"curl".equals(parsed.get(0).toLowerCase())) {
+            return new XTermResponse("Error: This terminal emulator only supports CURL");
+        } else {
+            String resp = $(strCmd);
+            return new XTermResponse(resp);
+        }
     }
 }
