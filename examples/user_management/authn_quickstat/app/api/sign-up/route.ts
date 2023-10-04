88bc257280f5655777242e4eb4ffc3b2429553c0
Gwen Shapira <cshapi@gmail.com>
2023-10-03 18:13:36 -0700

    made simple authn quickstart work again.

--- examples/user_management/authn_quickstat/app/api/sign-up/route.ts
+++ examples/user_management/authn_quickstat/app/api/sign-up/route.ts
@@ -1,5 +1,14 @@
 import { api } from '@/nile/Server';
 
 export async function POST(req: Request) {
-  return await api.auth.signUp(req);
-}
+
+  const res = await api.auth.signUp(req);
+
+  if (res && res.status >= 200 && res.status < 300) {
+      const body =  await res.json();
+      return new Response(JSON.stringify(body), { status: 201 });
+    } else  {
+      const body = await res.text();
+      return new Response(body, { status: res.status });
+  }
+} 
--- examples/user_management/authn_quickstat/nile/Server.ts
+++ examples/user_management/authn_quickstat/nile/Server.ts
@@ -1,9 +1,12 @@
 import Server from '@theniledev/server';
 
+
+// @ts-ignore // Nile requires a bit more configuration, but this example doesn't use those
+// Note that the Nile server configuration points to Nile APIs as the base path
 export const { api } = new Server({
-  workspace: String(process.env.NILE_WORKSPACE),
-  database: String(process.env.NILE_DATABASE),
+  workspace: String(process.env.NEXT_PUBLIC_WORKSPACE),
+  database: String(process.env.NEXT_PUBLIC_DATABASE),
   api: {
-    basePath: String(process.env.BASE_PATH),
+    basePath: String(process.env.NEXT_PUBLIC_NILE_API),
   },
 });
