59b79546ecbd3a4f934d2795f0a87a8594e41af4
Gwen Shapira <cshapi@gmail.com>
2023-10-06 14:07:35 -0700

    now deleting the right cookies during logout

--- examples/user_management/social_login_google/NextJS/app/api/logout/route.ts
+++ examples/user_management/social_login_google/NextJS/app/api/logout/route.ts
@@ -2,7 +2,8 @@ import { cookies } from 'next/headers'
 
 // Clean up the cookies and redirect to the home page
 export async function GET() {
-    cookies().delete('token');
+    cookies().delete('authData');
+    cookies().delete('errorData');
     return new Response(null, {
         headers: { 'Location': '/' },
         status: 302,    
