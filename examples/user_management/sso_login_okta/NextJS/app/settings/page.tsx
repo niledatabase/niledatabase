35c0fefac95d6a6f919349c21fef9779db1c12f9
Gwen Shapira <cshapi@gmail.com>
2023-10-06 18:38:35 -0700

    apply global style and layout

--- examples/user_management/sso_login_okta/NextJS/app/settings/page.tsx
+++ examples/user_management/sso_login_okta/NextJS/app/settings/page.tsx
@@ -13,13 +13,11 @@ async function getProviders() {
 export default async function SSOSettings() {
   const providers = await getProviders();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <SingleSignOnSettings
           providers={providers}
           callbackUrl={api.auth.getSSOCallbackUrl(headers())}
         />
       </div>
-    </main>
   );
 }
