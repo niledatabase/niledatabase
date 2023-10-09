967c562b9bbad105f11a781193441985ab50fce6
Gwen Shapira <cshapi@gmail.com>
2023-10-08 20:10:21 -0700

    added informative help message in Okta demo

--- examples/user_management/sso_login_okta/NextJS/app/settings/SingleSignOnSettings.tsx
+++ examples/user_management/sso_login_okta/NextJS/app/settings/SingleSignOnSettings.tsx
@@ -6,6 +6,8 @@ import Link from '@mui/joy/Link';
 import Stack from '@mui/joy/Stack';
 import Button from '@mui/joy/Button';
 import Box from '@mui/joy/Box';
+import Alert from '@mui/joy/Alert';
+import InfoIcon from '@mui/icons-material/Info';
 import { useRouter } from "next/navigation";
 
 export default function SingleSignOnSettings(props: OktaProps) {
@@ -19,6 +21,20 @@ export default function SingleSignOnSettings(props: OktaProps) {
           </Link>
         </Box>
         <Typography level="h1">Configure Okta</Typography>
+        <Alert
+          sx={{ alignItems: 'flex-start' }}
+          startDecorator={<InfoIcon />}
+          variant="soft"
+          color="neutral"
+        >
+          <div>
+            <div>Create an OKTA account to proceed with this demo. 
+          You can find step by step instructions in our <Link target="_blank" rel="noopener" href="https://niledatabase-www.vercel.app/docs/user-authentication/enterprise-login/okta#set-up-okta">Okta with Next.js guide</Link>.
+          </div>
+          </div>
+        </Alert>
+        <Typography level="body-lg">
+                    </Typography>
         <Okta {...props}
               onSuccess={() => {
                 push('/');
