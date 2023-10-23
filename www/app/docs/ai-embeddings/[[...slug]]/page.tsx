959dc41ac417a2acb5c912e9faa23ddd2f359d40
jrea <juzko.r@gmail.com>
2023-10-23 10:49:51 -0400

    chore: add metadata

--- www/app/docs/ai-embeddings/[[...slug]]/page.tsx
+++ www/app/docs/ai-embeddings/[[...slug]]/page.tsx
@@ -2,11 +2,31 @@ import PageContent from "@/app/docs/_components/PageContent";
 import { Param } from "@/app/docs/_components/PageContent/types";
 import { NavigationRoots } from "@/app/docs/_components/SideNavigation/types";
 import Container from "../../_components/Container";
-
-export default async function Page(props: { params: Param }) {
+import { ResolvingMetadata, Metadata } from "next";
+import findDocFile from "../../_utils/findDocFile";
+type Props = { params: Param };
+export default async function Page(props: Props) {
   return (
     <Container>
       <PageContent params={props.params} root={NavigationRoots.AI} />
     </Container>
   );
+}
+export async function generateMetadata(
+  props: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const { metadata } = await findDocFile({
+    ...props,
+    root: NavigationRoots.AI,
+  });
+
+  const previousImages = (await parent).openGraph?.images || [];
+  return {
+    title: metadata?.title,
+    description: metadata?.description,
+    openGraph: {
+      images: [...previousImages],
+    },
+  };
 }
--- www/app/docs/tenant-isolation/[[...slug]]/page.tsx
+++ www/app/docs/tenant-isolation/[[...slug]]/page.tsx
@@ -1,9 +1,11 @@
+import { ResolvingMetadata, Metadata } from "next";
 import Container from "../../_components/Container";
 import PageContent from "../../_components/PageContent";
 import { Param } from "../../_components/PageContent/types";
 import { NavigationRoots } from "../../_components/SideNavigation/types";
-
-export default async function Page(props: { params: Param }) {
+import findDocFile from "../../_utils/findDocFile";
+type Props = { params: Param };
+export default async function Page(props: Props) {
   return (
     <Container>
       <PageContent
@@ -12,4 +14,23 @@ export default async function Page(props: { params: Param }) {
       />
     </Container>
   );
+}
+
+export async function generateMetadata(
+  props: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const { metadata } = await findDocFile({
+    ...props,
+    root: NavigationRoots.TenantIsolation,
+  });
+
+  const previousImages = (await parent).openGraph?.images || [];
+  return {
+    title: metadata?.title,
+    description: metadata?.description,
+    openGraph: {
+      images: [...previousImages],
+    },
+  };
 }
