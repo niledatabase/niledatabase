0d8ad6f39a7651f7fa9aaca5b35a9876badc9012
Mircea Cadariu <cadariu.mircea@gmail.com>
2023-10-27 19:27:43 +0100

    Small fixes in the Java example (#203)
    * 'build-in' -> 'built-in'
    
    * Remove tenant parameter

--- examples/quickstart/java/src/main/java/com/example/todowebapp/controllers/TodoController.java
+++ examples/quickstart/java/src/main/java/com/example/todowebapp/controllers/TodoController.java
@@ -34,10 +34,10 @@ public class TodoController {
 
     // list all todos for a tenant
     @GetMapping
-    public @ResponseBody Iterable<Todo> getAllTodos(@PathVariable UUID tenant_id) {
+    public @ResponseBody Iterable<Todo> getAllTodos() {
         // Because we use a tenant-aware data source, this operation is safe
         // It will only return results for the tenant in the request
-        // We can use the build-in `findAll` and don't need to create a custom `findByTenantId` method for every Entity
+        // We can use the built-in `findAll` and don't need to create a custom `findByTenantId` method for every Entity
         return todoRepository.findAll();
     }
 }
