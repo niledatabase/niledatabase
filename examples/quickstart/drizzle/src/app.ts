de775e86ccab87b7dc35e0d733cdec358149bb05
Gwen Shapira <cshapi@gmail.com>
2023-11-13 22:06:26 -0800

    adding FE needed for template demo

--- examples/quickstart/drizzle/src/app.ts
+++ examples/quickstart/drizzle/src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import {tenantDB, tenantContext} from './db/db';
-import { tenants as tenantSchema, todos as todoSchema } from './db/schema';
+import {tenantDB, tenantContext, db} from './db/db';
+import { tenants as tenantSchema, todos as todoSchema, users as usersSchema, tenant_users } from './db/schema';
 import { eq } from "drizzle-orm";
 import { match } from "path-to-regexp";
+import expressBasicAuth from "express-basic-auth";
 
 const PORT = process.env.PORT || 3001;
+const REQUIRE_AUTH = process.env.REQUIRE_AUTH || false;
 
 const app = express();
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
@@ -22,27 +24,59 @@ app.use((req, res, next) => {
   tenantContext.run(tenantId, next);
 })
 
+// add basic auth middleware if required
+// we are doing this after setting the tenant context, so we'll have an appropriate connection to the DB
+app.use(
+  expressBasicAuth({authorizer: dbAuthorizer, authorizeAsync: true, unauthorizedResponse: getUnauthorizedResponse})
+);
 
-/*app.param('tenantId', (req, res, next, tenantId) => {
-  tenantContext.run(tenantId, next);
-});*/
+async function dbAuthorizer(username: string, password: string, cb: (err: any, result: boolean) => void) {
+  console.log('authenticating user: ' + username);
+  if (!REQUIRE_AUTH) {
+    return cb(null,true);
+  }
+  const users:any =  await tenantDB(async (tx) => {
+    return await tx.select().from(usersSchema).where(eq(usersSchema.id, username));
+  })
 
-// endpoint to create new tenants
+  if (users.length === 0) {
+    return cb(null,false);
+  }
+  return cb(null, true)
+}
+
+function getUnauthorizedResponse(req: any) {
+  return req.auth
+      ? ('Credentials ' + req.auth.user + ':' + req.auth.password + ' rejected')
+      : 'No credentials provided'
+}
+
+// create new tenant
 app.post("/api/tenants", async (req, res) => {
   try {
     const name = req.body.name;
     const id = req.body.id;
-    var tenant = null;
+    var tenants:any = null;
     if (id) {
-      tenant = await tenantDB(async (tx) => {
+      tenants = await tenantDB(async (tx) => {
         return await tx.insert(tenantSchema).values({name, id}).returning();
-      });
+      })
     } else {
-      tenant = await tenantDB(async (tx) => {
+      tenants = await tenantDB(async (tx) => {
         return await tx.insert(tenantSchema).values({name}).returning();
+      })
+    }
+    console.log("created tenant: " + JSON.stringify(tenants));
+    if (REQUIRE_AUTH) {
+      // need to connect user to tenant
+      // @ts-ignore
+      await tenantDB(async(tx) => {
+        // @ts-ignore
+        return await tx.insert(tenant_users).values({tenant_id: tenants[0].id, user_id: req.auth.user});
       });
     }
-    res.json(tenant);
+
+    res.json(tenants);
   } catch (error: any) {
     console.log("error creating tenant: " + error.message);
     res.status(500).json({
@@ -55,9 +89,18 @@ app.post("/api/tenants", async (req, res) => {
 app.get("/api/tenants", async (req, res) => {
   let tenants:any = [];
   try {
-      tenants = await tenantDB(async (tx) => {
-        return await tx.select().from(tenantSchema);
-      })
+      if (REQUIRE_AUTH) {
+       tenants = await tenantDB(async(tx) => {
+        return (db.select().from(tenantSchema)
+            .innerJoin(tenant_users, eq(tenantSchema.id,tenant_users.tenant_id))
+            // @ts-ignore
+            .where(eq(tenant_users.user_id, req.auth.user)));
+       })
+      } else {
+        tenants = await tenantDB(async(tx) => {
+          return await tx.select().from(tenantSchema);
+        })
+      }
       res.json(tenants);
   } catch (error: any) {
     console.log("error listing tenants: " + error.message);
@@ -120,9 +163,11 @@ app.get("/api/tenants/:tenantId/todos", async (req, res) => {
 // insecure endpoint to get all todos - don't try this in production 😅
 app.get("/insecure/all_todos", async (req, res) => {
   try {
+    console.log("getting all todos")
     const todos = await tenantDB(async (tx) => {
       return await tx.select().from(todoSchema);
     });
+    console.log("returning all todos: " + JSON.stringify(todos));
     res.json(todos);
   } catch (error: any) {
     console.log("error in insecure endpoint: " + error.message);
