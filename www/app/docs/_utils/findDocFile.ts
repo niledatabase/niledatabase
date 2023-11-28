de55a2856589ca47cdc6dc0829c232c9e302171f
jrea <juzko.r@gmail.com>
2023-11-28 14:45:19 -0500

    fix: use better titles for SEO

--- www/app/docs/_utils/findDocFile.ts
+++ www/app/docs/_utils/findDocFile.ts
@@ -9,48 +9,40 @@ type Metadata = {
 
 type ValidComponent = (props: MDXProps) => JSX.Element;
 
+export function getPage(params?: Param, page?: string) {
+  const possiblePage = params?.slug?.join("/");
+  if (possiblePage) {
+    return possiblePage;
+  }
+  return page;
+}
+
 export default async function findDocFile(props: Props): Promise<{
   metadata: Metadata;
   Component: void | ValidComponent;
+  path: void | string;
 }> {
-  const { params, page, root } = props;
+  const { params, root } = props;
+
+  const page = getPage(params, props.page);
+
   // on the index page
   if (params && Object.keys(params).length === 0) {
     try {
       const { metadata, default: Component } = (await import(
         `../${root}/[[...slug]]/index.mdx`
       )) as any;
-      return { metadata, Component };
+      return { metadata, Component, path: `./index.mdx` };
     } catch (e) {
       // its ok to not have in index page
     }
-  }
-  // in some requests, `page` is missing, so try and figure it out from the params
-  const possiblePage = params?.slug?.join("/");
-
-  try {
-    const { metadata, default: Component } = (await import(
-      `../${root}/[[...slug]]/${possiblePage}.mdx`
-    )) as any;
-    return { metadata, Component };
-  } catch (e) {
-    // try again
   }
 
   try {
     const { metadata, default: Component } = (await import(
       `../${root}/[[...slug]]/${page}.mdx`
     )) as any;
-    return { metadata, Component };
-  } catch (e) {
-    // try again
-  }
-
-  try {
-    const { metadata, default: Component } = (await import(
-      `../${root}/[[...slug]]/${possiblePage}/index.mdx`
-    )) as any;
-    return { metadata, Component };
+    return { metadata, Component, path: `./${page}.mdx` };
   } catch (e) {
     // try again
   }
@@ -59,11 +51,15 @@ export default async function findDocFile(props: Props): Promise<{
     const { metadata, default: Component } = await import(
       `../${root}/[[...slug]]/${page}/index.mdx`
     );
-    return { metadata, Component };
+    return {
+      metadata,
+      Component,
+      path: `./${page}/index.mdx`,
+    };
   } catch (e) {
     // do nothing
   }
 
   const _meta = {} as Metadata;
-  return { metadata: _meta, Component: undefined };
+  return { metadata: _meta, Component: undefined, path: undefined };
 }
