0db38c51c7981adf3dbb30d9ab17ae9b34ef38a6
Gwen Shapira <cshapi@gmail.com>
2024-01-19 17:35:04 -0800

    lol, forgot the actual docs

---
+++ www/app/docs/integrations/[[...slug]]/page.tsx
@@ -0,0 +1,33 @@
+import { ResolvingMetadata, Metadata } from "next";
+import Container from "../../_components/Container";
+import PageContent from "../../_components/PageContent";
+import { Param } from "../../_components/PageContent/types";
+import { NavigationRoots } from "../../_components/SideNavigation/types";
+import findDocFile from "../../_utils/findDocFile";
+type Props = { params: Param };
+export default async function Page(props: Props) {
+  return (
+    <Container>
+      <PageContent params={props.params} root={NavigationRoots.Integrations} />
+    </Container>
+  );
+}
+
+export async function generateMetadata(
+  props: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const { metadata } = await findDocFile({
+    ...props,
+    root: NavigationRoots.Integrations,
+  });
+
+  const previousImages = (await parent).openGraph?.images || [];
+  return {
+    title: metadata?.title,
+    description: metadata?.description,
+    openGraph: {
+      images: [...previousImages],
+    },
+  };
+}
