c9c8a963a04a9ec73a525d88516ada43bfd58a3f
jrea <juzko.r@gmail.com>
2024-03-25 11:35:48 -0400

    chore(repo): update deps to use 1.0.0 of nile-js

--- examples/user_management/sso_login_okta/NextJS/middleware.ts
+++ examples/user_management/sso_login_okta/NextJS/middleware.ts
@@ -1,14 +1,14 @@
-import { NextRequest } from 'next/server';
-import { NextResponse } from 'next/server';
-import middleware from '@theniledev/edge';
+import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import middleware from "@niledatabase/edge";
 
 // This function can be marked `async` if using `await` inside
 export default async function doMiddleware(request: NextRequest) {
   const response = NextResponse.next();
-  middleware(request, response, ['/api/:tenantId/users']);
+  middleware(request, response, ["/api/:tenantId/users"]);
   return response;
 }
 
 export const config = {
-  matcher: ['/api/:tenantId*', '/:tenantId*/users'],
+  matcher: ["/api/:tenantId*", "/:tenantId*/users"],
 };
