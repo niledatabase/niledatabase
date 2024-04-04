d8e1dad49abc92f9ba531a9350e889b4c7ff13ca
jrea <juzko.r@gmail.com>
2024-04-04 13:02:20 -0400

    fix(examples): update quickstart nextjs

--- examples/quickstart/nextjs/components/GoogleSSOForm.tsx
+++ examples/quickstart/nextjs/components/GoogleSSOForm.tsx
@@ -1,23 +1,20 @@
-'use client';
+"use client";
 // ^^^ this is the reason we need this wrapper component around GoogleLoginButton
 
-import React from 'react';
-import Stack from '@mui/joy/Stack';
-import { GoogleLoginButton, NileProvider } from '@niledatabase/react';
+import React from "react";
+import Stack from "@mui/joy/Stack";
+import { GoogleLoginButton, NileProvider } from "@niledatabase/react";
 
 export default function GoogleAuthPanel() {
-    {/* Note that in this case we tell the client-side component to talk to Nile directly, not the local API. 
-        This is specific for Google SSO */}
+  /* Note that in this case we tell the client-side component to talk to Nile directly, not the local API. 
+        This is specific for Google SSO */
   return (
-    <NileProvider basePath={process.env.NEXT_PUBLIC_NILE_API}>
-      <div style={{ maxWidth: '20rem', margin: '0 auto' }}>
+    <NileProvider basePath={`https://${process.env.NEXT_PUBLIC_NILEDB_API}`}>
+      <div style={{ maxWidth: "20rem", margin: "0 auto" }}>
         <Stack gap={2} justifyContent="center" alignItems="center">
-          <GoogleLoginButton
-            workspace={process.env.NEXT_PUBLIC_WORKSPACE}
-            database={process.env.NEXT_PUBLIC_DATABASE}
-          />
+          <GoogleLoginButton databaseId={process.env.NEXT_PUBLIC_NILEDB_ID} />
         </Stack>
       </div>
     </NileProvider>
-  )
+  );
 }
