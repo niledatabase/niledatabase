521d8dd17bc79e451343b721db1e6c7192b334c3
jrea <juzko.r@gmail.com>
2024-04-04 13:02:20 -0400

    fix(examples): update quickstart nextjs

--- examples/quickstart/nextjs/components/SignUpForm.tsx
+++ examples/quickstart/nextjs/components/SignUpForm.tsx
@@ -1,24 +1,27 @@
-'use client';
+"use client";
 
-import Stack from '@mui/joy/Stack';
-import Typography from '@mui/joy/Typography';
-import Link from '@mui/joy/Link';
-import Alert from '@mui/joy/Alert';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { NileProvider, UserSignupForm } from '@niledatabase/react';
+import Stack from "@mui/joy/Stack";
+import Typography from "@mui/joy/Typography";
+import Link from "@mui/joy/Link";
+import Alert from "@mui/joy/Alert";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { NileProvider, UserSignupForm } from "@niledatabase/react";
 
 export default function SignUp() {
   const [error, setError] = useState<string | null>(null);
   const { push } = useRouter();
   return (
-    <NileProvider basePath={process.env.NEXT_PUBLIC_BASE_PATH}>
-      <Stack gap={2} sx={{ maxWidth: '40ch' }}>
+    <NileProvider basePath={`http://localhost:3000/api`}>
+      <Stack gap={2} sx={{ maxWidth: "40ch" }}>
         <Typography level="h1">Sign up</Typography>
         {error && <Alert>{error}</Alert>}
         <UserSignupForm
-          onSuccess={(response) => push('/tenants')}
-          onError={() => {setError('an error has occurred.');}}/>
+          onSuccess={(response) => push("/tenants")}
+          onError={() => {
+            setError("an error has occurred.");
+          }}
+        />
         <p>
           Already a user? <Link href="/">Log in here</Link>
         </p>
