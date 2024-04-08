7c9509a28aa149c6146f428bb1fe67440b369d56
jrea <juzko.r@gmail.com>
2024-04-08 12:17:48 -0400

    fix(examples): update email_login

--- examples/user_management/email_login/NextJS/nile/Server.ts
+++ examples/user_management/email_login/NextJS/nile/Server.ts
@@ -1,11 +1,5 @@
-import Server from "@niledatabase/server";
+import Nile from "@niledatabase/server";
 
 // @ts-ignore // Nile requires a bit more configuration, but this example doesn't use those
 // Note that the Nile server configuration points to Nile APIs as the base path
-export const { api } = new Server({
-  workspace: String(process.env.NEXT_PUBLIC_WORKSPACE),
-  database: String(process.env.NEXT_PUBLIC_DATABASE),
-  api: {
-    basePath: String(process.env.NEXT_PUBLIC_NILE_API),
-  },
-});
+export const { api } = await Nile();
