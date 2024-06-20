aacce9131a846fe911eb825256200173c20a5216
Gwen Shapira <cshapi@gmail.com>
2024-06-19 20:50:34 -0700

    removing unnecessary configs and unsupported syntax

--- examples/ai/code_assist/next.config.mjs
+++ examples/ai/code_assist/next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // (Optional) Export as a standalone site
-    // See https://nextjs.org/docs/pages/api-reference/next-config-js/output#automatically-copying-traced-files
-    output: 'standalone', // Feel free to modify/remove this option
-    
-    // Indicate that these packages should not be bundled by webpack
-    experimental: {
-        serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
-    },
+
 };
 
 export default nextConfig;
