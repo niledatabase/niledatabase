c7ebb621eb36285c392ab90d430c0b4a570ba17a
Gwen Shapira <cshapi@gmail.com>
2024-06-19 21:30:11 -0700

    brought back the lost styling

--- examples/ai/code_assist/src/app/tenants/[tenantid]/ask/page.tsx
+++ examples/ai/code_assist/src/app/tenants/[tenantid]/ask/page.tsx
@@ -7,6 +7,7 @@ import Grid from '@mui/joy/Grid';
 import Input from '@mui/joy/Input';
 import Typography from '@mui/joy/Typography';
 import Highlight from 'react-highlight';
+import styles from "../../page.module.css";
 
 
 
