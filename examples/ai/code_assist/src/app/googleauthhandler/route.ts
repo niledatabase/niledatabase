26ab662125496f3a06cd0c57a1ac176a0fb0ee44
Gwen Shapira <cshapi@gmail.com>
2024-07-03 15:34:08 -0700

    more ui improvements + auth fixes

--- examples/ai/code_assist/src/app/googleauthhandler/route.ts
+++ examples/ai/code_assist/src/app/googleauthhandler/route.ts
@@ -1,7 +1,8 @@
 import { jwtDecode } from "jwt-decode";
 import { cookies } from "next/headers";
 import { NileJWTPayload, cookieOptions, toCookieData } from "@/lib/AuthUtils";
-import {Nile} from "@niledatabase/server";
+import { registerTenants } from "@/lib/TenantRegistration";
+
 
 // Google SSO redirect posts Nile's response to this route
 // This route then redirects the user to the appropriate page
@@ -45,15 +46,9 @@ async function redirectOnSuccess(formData: FormData): Promise<string> {
     const accessToken = String(formData.get("access_token"));
     const decodedJWT = jwtDecode<NileJWTPayload>(accessToken);
     const cookieData = toCookieData(formData, decodedJWT);
-    const event = formData.get("event");
+
     const user_id = decodedJWT.sub;
-    const nile = await Nile();
-    const tenants = await nile.db.query("select id from tenants where id not in (select tenant_id from users.tenant_users where user_id = $1)", [user_id]);
-    for (const tenant of tenants.rows) {
-      await nile.db.query(
-        "INSERT INTO users.tenant_users(user_id, tenant_id) VALUES($1, $2)",
-        [user_id, tenant.id]);
-    }
+    await registerTenants(user_id);
     
     cookies().set("authData", JSON.stringify(cookieData), cookieOptions(3600));
 
