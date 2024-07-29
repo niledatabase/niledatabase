a37462da0c2ba10cd940cd888d853341ae042cb5
Gwen Shapira <cshapi@gmail.com>
2024-07-29 13:42:30 -0700

    more prettier

--- examples/ai/ai_pdf/app/(marketing)/page.tsx
+++ examples/ai/ai_pdf/app/(marketing)/page.tsx
@@ -12,7 +12,7 @@ const MarketingPage = () => {
     <div className="flex items-center justify-between flex-col">
       <div className={cn("flex items-center justify-center flex-col")}>
         <h1 className="text-3xl md:text-6xl text-center mb-6 font-switzerBold">
-           PDF search assistant for your organization
+          PDF search assistant for your organization
         </h1>
         <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
           work forward.
