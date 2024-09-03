09611bca850dbf98542a6a0a90b05c340c150902
Gwen Shapira <cshapi@gmail.com>
2024-09-02 19:13:11 -0700

    first ui - login, signup, tenant selection

---
+++ examples/ai/sales_insight/ui/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), svgr()],
+  server: {
+    proxy: {
+      // string shorthand: http://localhost:5173/foo -> http://localhost:4567/foo
+      // This has to be overridden for production
+      "/api": {
+        target: "https://gwenshap--sales-insight-web-fastapi-app-dev.modal.run",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
