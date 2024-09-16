afd55f2226ff072707797c9ef14773652ccfaf0a
sriramsub <sriram.sub@gmail.com>
2024-09-16 00:39:22 -0700

    update

--- www/app/pricing/_components/Free.tsx
+++ www/app/pricing/_components/Free.tsx
@@ -8,7 +8,7 @@ export default function Free() {
     <Column
       tier={"Free"}
       price={0}
-      tierSizzle={"For prototyping or side projects"}
+      tierSizzle={"For prototyping or side projects. No pauses, always available"}
       computeTop="50 million query tokens"
       computeSub="$0.06 per million additional tokens"
       storageTop="1 GB"
