ed613537cb364590e782f7c89bae4d5d6bc8b728
Joseph Rea <jrea@users.noreply.github.com>
2024-09-19 11:26:01 -0400

    fix(www): fix blog look (#483)

--- www/scripts/blog/generateSearch.mjs
+++ www/scripts/blog/generateSearch.mjs
@@ -16,7 +16,6 @@ const client = algoliasearch(
 async function upload(output) {
   return new Promise((resolve) => {
     const index = client.initIndex("blog");
-    algoliasearch;
     index
       .saveObjects(output)
       .then(() => {
