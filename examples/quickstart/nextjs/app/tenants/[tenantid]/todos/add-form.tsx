62d89dba8a15cfe889526bb0831fb63ae737b913
Gwen Shapira <cshapi@gmail.com>
2024-09-28 19:32:16 -0700

    Add timing information to show why add-task in our AI-native NextJS example takes some time (#507)
    * add timing information to show why add-task is taking a while
    
    * better wording
    
    * prettier

--- examples/quickstart/nextjs/app/tenants/[tenantid]/todos/add-form.tsx
+++ examples/quickstart/nextjs/app/tenants/[tenantid]/todos/add-form.tsx
@@ -3,11 +3,16 @@
 import IconButton from "@mui/joy/IconButton";
 import Add from "@mui/icons-material/Add";
 import Input from "@mui/joy/Input";
+import Snackbar from "@mui/joy/Snackbar";
+import Alert from "@mui/joy/Alert";
+import CheckIcon from "@mui/icons-material/Check";
 import ListItermDecorator from "@mui/joy/ListItemDecorator";
 import Box from "@mui/joy/Box";
+import Typography from "@mui/joy/Typography";
 // @ts-expect-error -- useFormState is new and lacks type definitions
 import { experimental_useFormState as useFormState } from "react-dom";
 import { addTodo } from "./todo-actions";
+import { useState, useEffect } from "react";
 
 const initialState = {
   message: null,
@@ -16,6 +21,17 @@ const initialState = {
 export function AddForm({ tenantid }: { tenantid: string }) {
   const addTodoWithTenant = addTodo.bind(null, tenantid);
   const [state, formAction] = useFormState(addTodoWithTenant, initialState);
+  const [isSnackbarOpen, setIsSnackbarOpen] = useState(false);
+
+  useEffect(() => {
+    if (state.message !== null) {
+      setIsSnackbarOpen(true);
+    }
+  }, [state.message]);
+
+  const handleSnackbarClose = () => {
+    setIsSnackbarOpen(false);
+  };
 
   return (
     <form
@@ -35,9 +51,20 @@ export function AddForm({ tenantid }: { tenantid: string }) {
         name="todo"
         sx={{ width: "95%" }}
       ></Input>
-      <p aria-live="polite" className="sr-only" role="status">
-        {state?.message}
-      </p>
+      <br />
+      <Snackbar
+        open={isSnackbarOpen}
+        onClose={handleSnackbarClose}
+        autoHideDuration={30000}
+      >
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+          {Object.entries(state).map(([key, value]) => (
+            <Typography key={key} level="body-md" textAlign="left">
+              {key}: {String(value)}
+            </Typography>
+          ))}
+        </Box>
+      </Snackbar>
     </form>
   );
 }
