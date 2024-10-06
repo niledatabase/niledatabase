88f5bbb7e496f9f05f179ec410f56f4401d4526e
sriramsub <sriram.sub@gmail.com>
2024-10-06 08:06:40 -0700

    update

--- www/app/_components/common/Hero/index.tsx
+++ www/app/_components/common/Hero/index.tsx
@@ -34,7 +34,7 @@ export function HeroText() {
         </div>
         <div>B2B apps fast</div>
       </h1>
-      <h2 className="text-[40px] leading-6">
+      <h2 className="text-[32px] leading-[32px]">
         PostgreSQL re-engineered for multi-tenant apps
       </h2>
       <div className="flex gap-4 items-center justify-start">
