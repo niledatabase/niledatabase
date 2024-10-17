4fabb12b6e0f41021da31fdeba71b3da5a92c64f
jrea <juzko.r@gmail.com>
2024-10-17 12:03:36 -0400

    feat(examples): update nextjs email to v3

---
+++ examples/user_management/email_login/NextJS/components/ui/SignInForm.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { SignInForm } from "@niledatabase/react";
+import { useRouter } from "next/navigation";
+import { Button } from "./button";
+import Link from "next/link";
+
+export default function Login() {
+  const { push } = useRouter();
+  return (
+    <div className="flex flex-col gap-4 mt-24">
+      <SignInForm callbackUrl="/dashboard" />
+      <p>
+        Not a user yet?{" "}
+        <Button variant="link" className={"pl-0"}>
+          <Link href="/sign-up">Sign up here</Link>
+        </Button>
+      </p>
+    </div>
+  );
+}
