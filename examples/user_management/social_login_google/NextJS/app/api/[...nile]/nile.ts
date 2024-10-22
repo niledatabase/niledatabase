afd537b7c2392c3cd1d6558d134ca6c2f7ff6ca0
Joseph Rea <jrea@users.noreply.github.com>
2024-10-22 08:37:11 -0400

    fix(examples): get google auth to work (#531)

---
+++ examples/user_management/social_login_google/NextJS/app/api/[...nile]/nile.ts
@@ -0,0 +1,8 @@
+import "server-only";
+import NileServer from "@niledatabase/server";
+const _nile = await NileServer({
+  secureCookies: process.env.VERCEL === "1",
+});
+
+export const { handlers } = _nile.api;
+export const nile = _nile;
--- examples/user_management/social_login_google/NextJS/app/dashboard/page.tsx
+++ examples/user_management/social_login_google/NextJS/app/dashboard/page.tsx
@@ -1,47 +1,88 @@
-import styles from "../page.module.css";
-
 import { cookies } from "next/headers";
-import AuthCookieData from "@/app/model/AuthCookieData";
-import Typography from "@mui/joy/Typography";
-import MUILink from "@mui/joy/Link";
-import NextLink from "next/link";
-import AuthDataPanel from "@/app/dashboard/AuthDataPanel";
+import { nile } from "../api/[...nile]/nile";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import SignOutButton from "./SignOutButton";
+import { Tenant, JWT } from "@niledatabase/server";
+import { Ban } from "lucide-react";
 
-export default function Dashboard() {
-  const authCookie = cookies().get("authData");
-  const authData = authCookie
-    ? (JSON.parse(authCookie.value) as AuthCookieData)
-    : null;
+function Carder({
+  children,
+}: {
+  children: Array<JSX.Element | string | undefined | number | null>;
+}) {
+  return (
+    <div className="flex flex-row gap-4 justify-between">
+      <div className="min-w-24">{children[0]}</div>
+      <div className="text-left flex-1">{children[1]}</div>
+    </div>
+  );
+}
+type TenantResponseType = undefined | Response | Tenant;
+export default async function Dashboard() {
+  const nextCookies = cookies();
+  nile.api.headers = new Headers({ cookie: nextCookies.toString() });
 
-  // Bail out early if the user is not authenticated
-  if (!authData) {
+  const currentUser = await nile.api.users.me();
+
+  if (currentUser instanceof Response) {
     return (
-      <div>
-        <Typography>No authentication data found in cookies.</Typography>
-        <Typography>
-          {" "}
-          <MUILink component={NextLink} href="/">
-            {" "}
-            Try logging in first{" "}
-          </MUILink>{" "}
-        </Typography>
+      <div className="mt-24 flex flex-col gap-5">
+        <div className="flex flex-col gap-3">
+          <div className="text-4xl flex flex-row gap-2 items-center">
+            <Ban className="stroke-red-600" size={40} /> Unable to get user
+            information
+          </div>
+          <div>Reason: {await currentUser.text()}</div>
+        </div>
+        <div>
+          <SignOutButton text="Back to Sign in" />
+        </div>
       </div>
     );
   }
 
-  // we get tenant ID a bit differently on sign up vs login events
-  // we'll normalize it a bit here so the downstream code doesn't have to worry about it
-  if (authData.event === "LOGIN") {
-    const allAud = authData.tokenData?.aud; // the audiences may include a tenant ID
-    if (Array.isArray(allAud)) {
-      // the audience includes 'nile', 'database:xxxxx' (where xxxxx is the database id), and
-      // 'tenant:xxxxx' (where xxxxx is the tenant ID)
-      const tenant = allAud.find((claim) => claim.startsWith("tenant:"));
-      authData.tenantId = tenant?.split(":")[1] || "";
-    }
+  const requests: [JWT, TenantResponseType] = [
+    nile.api.auth.session() as unknown as JWT,
+    undefined,
+  ];
+  if (currentUser.tenants.length) {
+    requests[1] = nile.api.tenants.getTenant(
+      currentUser.tenants[0].id
+    ) as unknown as Tenant;
   }
+  const [session, tenant] = await Promise.all(requests);
 
-  // if we are here, we have auth data and likely have a tenant ID too
-  // regardless, lets show what we have
-  return <AuthDataPanel authData={authData} />;
+  return (
+    <div className="flex flex-col gap-4 mt-24">
+      {!(tenant instanceof Response) ? (
+        <Card>
+          <CardHeader>Tenant information</CardHeader>
+          <CardContent>
+            <Carder>Tenant id {tenant?.id}</Carder>
+            <Carder>Tenant name {tenant?.name}</Carder>
+          </CardContent>
+        </Card>
+      ) : null}
+      <Card>
+        <CardHeader>User information</CardHeader>
+        <CardContent>
+          <Carder>id {currentUser.id}</Carder>
+          <Carder>name {currentUser.givenName}</Carder>
+          <Carder>email {currentUser.email}</Carder>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>Token information</CardHeader>
+        <CardContent>
+          <Carder>email {session.email}</Carder>
+          <Carder>id {session.id}</Carder>
+          <Carder>Subject {session.sub}</Carder>
+          <Carder>Expiration {session.exp}</Carder>
+          <Carder>Issued at {session.iat}</Carder>
+          <Carder>JWT ID {session.jti}</Carder>
+        </CardContent>
+      </Card>
+      <SignOutButton />
+    </div>
+  );
 }
