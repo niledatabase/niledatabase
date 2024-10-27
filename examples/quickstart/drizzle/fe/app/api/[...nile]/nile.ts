91f8423dec57cb2c2f1be9b2c9b20a0a752dc59b
Gwen Shapira <cshapi@gmail.com>
2024-10-27 15:31:15 -0700

    sdk v3 without styling (#536)
    * sdk v3 without styling
    
    * added tailwind
    
    * fixed styling
    
    * missed a component
    
    * removed debug logging and another missing component

---
+++ examples/quickstart/drizzle/fe/app/api/[...nile]/nile.ts
@@ -0,0 +1,8 @@
+import "server-only";
+import NileServer from "@niledatabase/server";
+const _nile = await NileServer({
+  secureCookies: process.env.VERCEL === "1",
+//  debug: true,
+});
+export const { handlers } = _nile.api;
+export const nile = _nile;
