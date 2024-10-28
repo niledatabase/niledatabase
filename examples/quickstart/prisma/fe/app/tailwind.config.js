55567d00ec9641e00865de65dc0f86367c7414f5
Gwen Shapira <cshapi@gmail.com>
2024-10-28 12:13:02 -0700

    Revert "removed more animation"
    This reverts commit 57c5206ea5b5063b272c652e995a64394dc1a278.

--- examples/quickstart/prisma/fe/app/tailwind.config.js
+++ examples/quickstart/prisma/fe/app/tailwind.config.js
@@ -68,6 +68,10 @@ module.exports = {
           to: { height: "0" },
         },
       },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   }
 };
