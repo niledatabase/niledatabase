57c5206ea5b5063b272c652e995a64394dc1a278
Gwen Shapira <cshapi@gmail.com>
2024-10-27 21:31:27 -0700

    removed more animation

--- examples/quickstart/prisma/fe/app/tailwind.config.js
+++ examples/quickstart/prisma/fe/app/tailwind.config.js
@@ -68,10 +68,6 @@ module.exports = {
           to: { height: "0" },
         },
       },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
     },
   }
 };
