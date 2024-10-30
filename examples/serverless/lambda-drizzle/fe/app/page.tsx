bb5a556cca3f9b7ef93cbe4109ea36440c1a4f43
Gwen Shapira <cshapi@gmail.com>
2024-10-30 15:17:32 -0700

    update to use v3 sdk

--- examples/serverless/lambda-drizzle/fe/app/page.tsx
+++ examples/serverless/lambda-drizzle/fe/app/page.tsx
@@ -1,22 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Typography from "@mui/joy/Typography";
-import Stack from "@mui/joy/Stack";
-import GoogleAuthPanel from "@/components/GoogleSSOForm";
-import ErrorBox from "@/components/ErrorBox";
+import { Suspense } from 'react';
+import GoogleAuthPanel from "../components/GoogleSSOForm";
 
 export default function Home() {
-  const authType = process.env.AUTH_TYPE;
   return (
-    <div>
-      <div className={styles.center}>
-        <Stack gap={5} sx={{ maxWidth: "40rem" }} alignItems={"center"}>
-          <ErrorBox />
-          {/* These components are simple wrappers around Nile's React components. 
-           It is needed because Nile's React components are client-side only. */}
-          <GoogleAuthPanel />
-        </Stack>
-      </div>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <GoogleAuthPanel />
+    </Suspense>
   );
 }
