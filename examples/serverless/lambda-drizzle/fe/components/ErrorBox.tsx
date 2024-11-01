775c740ff9b5a9ffe8f66e942865c31a815a932f
Gwen Shapira <cshapi@gmail.com>
2024-10-31 19:02:22 -0700

    missing file

---
+++ examples/serverless/lambda-drizzle/fe/components/ErrorBox.tsx
@@ -0,0 +1,20 @@
+import { cookies } from "next/headers";
+import { Alert } from "@mui/joy";
+import Stack from "@mui/joy/Stack";
+import Typography from "@mui/joy/Typography";
+
+export default async function ErrorBox() {
+  const cookieStore = await cookies();
+  const errorCookie = cookieStore.get("errorData");
+  const errorData = errorCookie ? JSON.parse(errorCookie.value) : null;
+
+  return errorData ? (
+    <Stack sx={{ mb: 2 }}>
+      <Alert sx={{ mb: 1 }} color="danger">
+        An error occurred during sign-in.
+      </Alert>
+      <Typography level="title-sm">Error details:</Typography>
+      <Typography level="body-sm">{errorData?.message}</Typography>
+    </Stack>
+  ) : null;
+}
