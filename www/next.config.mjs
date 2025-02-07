9cb6ff49f1fca6c3bb745bb7d9c5e2a63674102d
sriramsub <sriram.sub@gmail.com>
2025-02-07 15:00:14 -0800

    fix format

--- www/next.config.mjs
+++ www/next.config.mjs
@@ -5,27 +5,25 @@ import rehypeHighlight from "rehype-highlight";
 import scala from "highlight.js/lib/languages/scala";
 import sql from "highlight.js/lib/languages/sql";
 
-module.exports = {
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: '/docs',
-          destination: 'https://nile.mintlify.dev/docs',
-        },
-        {
-          source: '/docs/',
-          destination: 'https://nile.mintlify.dev/docs/',
-        },
-      ],
-      afterFiles: [
-        {
-          source: '/docs/:match*',
-          destination: 'https://nile.mintlify.dev/docs/:match*',
-        },
-      ],
-    };
-  },
+const rewrites = async () => {
+  return {
+    beforeFiles: [
+      {
+        source: '/docs',
+        destination: 'https://nile.mintlify.dev/docs',
+      },
+      {
+        source: '/docs/',
+        destination: 'https://nile.mintlify.dev/docs/',
+      },
+    ],
+    afterFiles: [
+      {
+        source: '/docs/:match*',
+        destination: 'https://nile.mintlify.dev/docs/:match*',
+      },
+    ],
+  };
 };
 
 const withMdx = nextMdx({
@@ -50,6 +48,9 @@ const withMdx = nextMdx({
 const nextConfig = {
   // Configure page extensions
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  
+  // Add rewrites configuration
+  rewrites,
   
   // Exclude docs from the build
   webpack: (config, { isServer }) => {
