b0cd5f0438d94fa23ac9657c5861a8949e64243a
jrea <juzko.r@gmail.com>
2025-03-08 12:20:38 -0500

    fix(examples): upgrade to v3 stable on reset password

--- examples/user_management/reset_password/NextJS/app/page.tsx
+++ examples/user_management/reset_password/NextJS/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-items-center pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <PasswordResetRequestForm callbackURL="http://localhost:3000/reset-password" />
+      <PasswordResetRequestForm callbackUrl="http://localhost:3000/reset-password" />
       <span>
         Not a user yet?{" "}
         <Link className="underline text-primary" href="/sign-up">
