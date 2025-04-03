53d529e1b70dfce8d109f416f5449954204bc46f
jrea <juzko.r@gmail.com>
2025-04-03 10:32:11 -0400

    fix(examples): restore secure cookies on vercel

--- examples/quickstart/nextjs/app/api/[...nile]/nile.ts
+++ examples/quickstart/nextjs/app/api/[...nile]/nile.ts
@@ -1,7 +1,12 @@
 import { Nile } from "@niledatabase/server";
 
 export const nile = await Nile({
+<<<<<<< HEAD
   //  debug: true,
+=======
+  secureCookies: process.env.VERCEL === "1",
+  debug: true,
+>>>>>>> b092584f (fix(examples): restore secure cookies on vercel)
 });
 
 export const { handlers } = nile.api;
