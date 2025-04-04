ad62e691d2a528aacde87e4d3a542193bb7be002
jrea <juzko.r@gmail.com>
2025-04-04 09:15:54 -0400

    chore: remove unused code

--- examples/ai/ai_pdf/app/(main)/dashboard/organization/[organizationId]/chat/[fileId]/_components/empty.tsx
+++ examples/ai/ai_pdf/app/(main)/dashboard/organization/[organizationId]/chat/[fileId]/_components/empty.tsx
@@ -3,7 +3,6 @@ import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { UseChatHelpers } from "ai/react";
 import { ArrowRight } from "lucide-react";
-import { useEffect, useState } from "react";
 
 const exampleMessages = [
   {
@@ -17,30 +16,30 @@ const exampleMessages = [
 ];
 
 export function EmptyStateAI({ setInput }: Pick<UseChatHelpers, "setInput">) {
-
   return (
     <>
-      <div className="text-2xl font-ranadeRegular"> Welcome! Ask a question related to this document.  </div>
-      {isTypingDone && (
-        <Card className="p-5 bg-muted font-ranadeLight">
-          <div className="flex flex-col items-center w-full justify-center">
-            <p className="w-full mt-3">converse.ai</p>
-          </div>
-          <div className="mt-4 flex flex-col items-start space-y-2">
-            {exampleMessages.map((message, index) => (
-              <Button
-                key={index}
-                variant="link"
-                className="h-auto p-0 text-base"
-                onClick={() => setInput(message.message)}
-              >
-                <ArrowRight className="mr-2 text-muted-foreground" />
-                {message.heading}
-              </Button>
-            ))}
-          </div>
-        </Card>
-      )}
+      <div className="text-2xl font-ranadeRegular">
+        {" "}
+        Welcome! Ask a question related to this document.{" "}
+      </div>
+      <Card className="p-5 bg-muted font-ranadeLight">
+        <div className="flex flex-col items-center w-full justify-center">
+          <p className="w-full mt-3">converse.ai</p>
+        </div>
+        <div className="mt-4 flex flex-col items-start space-y-2">
+          {exampleMessages.map((message, index) => (
+            <Button
+              key={index}
+              variant="link"
+              className="h-auto p-0 text-base"
+              onClick={() => setInput(message.message)}
+            >
+              <ArrowRight className="mr-2 text-muted-foreground" />
+              {message.heading}
+            </Button>
+          ))}
+        </div>
+      </Card>
     </>
   );
 }
