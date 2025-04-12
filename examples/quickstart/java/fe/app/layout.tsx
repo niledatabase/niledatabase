d8c7ae328e5c490a89fa0cff3647317a881a87b3
Gwen Shapira <cshapi@gmail.com>
2025-04-11 18:34:59 -0700

    more missing files

--- examples/quickstart/java/fe/app/layout.tsx
+++ examples/quickstart/java/fe/app/layout.tsx
@@ -87,7 +87,6 @@ export default function RootLayout({
                     width={49.125}
                     height={60}
                   />
-                  Drizzle
                 </Link>
                 Getting started guide
               </div>
