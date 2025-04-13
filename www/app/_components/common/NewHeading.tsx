71deab7871ae2e2918dd6f912970d7857020bd71
sriramsub <sriram.sub@gmail.com>
2025-04-13 14:15:57 -0700

    update size

--- www/app/_components/common/NewHeading.tsx
+++ www/app/_components/common/NewHeading.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 export function NewHeading({ children }: { children: JSX.Element | string }) {
   return (
-    <h2 className="text-[24px] leading-[28px] sm:text-[42px] sm:leading-[42px] xl:text-[64px] xl:leading-[64px] lg:leading-[50px] lg:text-[50px] text-center w-5/6 2xl:w-2/3 -tracking-[0.64px]">
+    <h2 className="text-[29px] leading-[34px] sm:text-[42px] sm:leading-[42px] xl:text-[64px] xl:leading-[64px] lg:leading-[50px] lg:text-[50px] text-center w-5/6 2xl:w-2/3 -tracking-[0.64px]">
       {children}
     </h2>
   );
