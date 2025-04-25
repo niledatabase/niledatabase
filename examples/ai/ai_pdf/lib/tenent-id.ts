f88183d948c4ef6df4907a70dce62693a487edf8
Gwen Shapira <cshapi@gmail.com>
2025-04-24 20:43:09 -0700

    headers are now a promise

--- examples/ai/ai_pdf/lib/tenent-id.ts
+++ examples/ai/ai_pdf/lib/tenent-id.ts
@@ -2,7 +2,7 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export const currentTenantId = async () => {
-  const headersList = headers();
+  const headersList = await headers();
   console.log(headersList);
   const referer = headersList.get("referer");
   console.log(referer);
