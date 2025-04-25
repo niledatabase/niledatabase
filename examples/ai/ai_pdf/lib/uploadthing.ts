73f5e7a98d354689c495b1114952cfdeb2b89356
Gwen Shapira <cshapi@gmail.com>
2025-04-24 18:16:55 -0700

    remove deprecated uploadthing import

--- examples/ai/ai_pdf/lib/uploadthing.ts
+++ examples/ai/ai_pdf/lib/uploadthing.ts
@@ -1,4 +1,4 @@
-import { generateReactHelpers } from "@uploadthing/react/hooks";
+import { generateReactHelpers } from "@uploadthing/react";
 
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
 
