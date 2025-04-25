567b5e1aa5af4ab45ad9dd6a749cc8f3d175d387
Gwen Shapira <cshapi@gmail.com>
2025-04-24 18:29:14 -0700

    update another deprecated uploadthing import

--- examples/ai/ai_pdf/app/api/uploadthing/route.ts
+++ examples/ai/ai_pdf/app/api/uploadthing/route.ts
@@ -1,9 +1,9 @@
-import { createNextRouteHandler } from "uploadthing/next";
+import { createRouteHandler } from "uploadthing/next";
 
 import { ourFileRouter } from "./core";
 
 // export const maxDuration = 30;
 // Export routes for Next App Router
-export const { GET, POST } = createNextRouteHandler({
+export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
 });
