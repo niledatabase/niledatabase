ec437a64e4ebe33d4c968be4254a5d4fda6fb687
Gwen Shapira <cshapi@gmail.com>
2025-04-29 17:16:18 -0700

    upgrade to nile sdk 4.2 and use handlersWithContext

--- examples/ai/code_assist/src/app/api/auth/callback/google/route.ts
+++ examples/ai/code_assist/src/app/api/auth/callback/google/route.ts
@@ -1,22 +1,15 @@
 import { NextRequest } from "next/server";
-import { nile, handlers } from "../../../[...nile]/nile";
+import { handlersWithContext } from "../../../[...nile]/nile";
 import { registerTenants } from "@/lib/TenantRegistration";
 
 export async function GET(req: NextRequest) {
-  const postHandled = await handlers.GET(req);
+  const { nile, response } = await handlersWithContext.GET(req);
 
-  if (postHandled) {
-    const setCookie = postHandled.headers.getSetCookie();
-    const hasSession = setCookie.filter((c) =>
-      c.includes("nile.session-token")
-    );
-    if (hasSession) {
-      nile.api.headers = new Headers({ cookie: hasSession.toString() });
-      const me = await nile.api.users.me();
-      if ("id" in me) {
-        await registerTenants(me.id);
-      }
+  if (nile) {
+    const me = await nile.api.users.me();
+    if ("id" in me) {
+      await registerTenants(me.id);
     }
   }
-  return postHandled;
+  return response;
 }
