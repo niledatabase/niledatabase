8205acf57bb3648f7a601153606e547c7a43c25f
jrea <juzko.r@gmail.com>
2025-06-26 12:00:10 -0400

    chore(examples): node_react to v5

--- examples/quickstart/node_react/src/be/app.ts
+++ examples/quickstart/node_react/src/be/app.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
-import express from "express";
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express, { Application, RequestHandler } from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import {
   embedTask,
@@ -11,73 +11,29 @@ import {
   embeddingToSQL,
 } from "./AiUtils.js";
 import { Nile } from "@niledatabase/server";
-import { NileExpressHandler } from "@niledatabase/server/express";
+import { express as nileExpress } from "@niledatabase/express";
 
 const fe_url = process.env.FE_URL || "http://localhost:3006";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const nile = await Nile({
-  api: {
-    secureCookies: process.env.VERCEL === "1",
-    origin: fe_url,
-  },
-  debug: true,
-});
-
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Serve static files from the Vite React app build directory
-app.use(express.static(path.join(__dirname, '../fe')));
-
-const { paths, handler } = await NileExpressHandler(nile, {
-  muteResponse: true,
+const nile = Nile({
+  origin: fe_url,
+  debug: true,
+  extensions: [nileExpress(app)],
 });
-app.get(paths.get, handleRoutes);
-app.post(paths.post, handleRoutes);
-app.put(paths.put, handleRoutes);
-app.delete(paths.delete, handleRoutes);
 
-async function handleRoutes(
-  req: Request & { originalUrl: string; url: string },
-  res: any,
-  next: any
-) {
-  try {
-    // because a proxy is used, we must re-create the valid that goes to the FE
-    req.url = `${fe_url}${req.originalUrl}`;
-    console.log("request headers", req.headers);
-    const response = await handler(req);
-
-    if (response) {
-      const { status, headers, body } = response;
-      console.log("response from nile:");
-      console.log(status, headers, body);
-      res.status(status).set(headers);
-      if (typeof body === "string") {
-        res.send(body);
-      } else {
-        res.json(body ?? {});
-      }
-    }
-  } catch (error) {
-    console.error("Error handling request:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-}
+// Serve static files from the Vite React app build directory
+app.use(express.static(path.join(__dirname, "../fe")));
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
-// set the tenant ID in the context based on the URL parameter - this runs after the auth middleware
-app.param("tenantId", (req, res, next, tenantId) => {
-  nile.tenantId = tenantId;
-  next();
-});
 
 // add new task for tenant
 app.post("/api/tenants/:tenantId/todos", async (req, res) => {
@@ -103,23 +59,27 @@ app.post("/api/tenants/:tenantId/todos", async (req, res) => {
     EmbeddingTasks.SEARCH_DOCUMENT
   ).catch((e) => {
     console.error(e);
-    return [0, 0, 0];
+    return new Array(768).fill(0);
   });
 
-  const newTodo = await nile.db.query(
-    `INSERT INTO todos (tenant_id, title, complete, estimate, embedding)
+  try {
+    const newTodo = await nile.db.query(
+      `INSERT INTO todos (tenant_id, title, complete, estimate, embedding)
       VALUES ($1, $2, $3, $4, $5)
       RETURNING *;`,
-    [
-      nile.tenantId, // could also be req.params.tenantId, but see `app.param("tenantId")`
-      title,
-      complete || false,
-      estimate,
-      embeddingToSQL(embedding),
-    ]
-  );
+      [
+        nile.getContext().tenantId, // could also be req.params.tenantId, but express extension overrides get/set context via AsyncLocalStorage
+        title,
+        complete || false,
+        estimate,
+        embeddingToSQL(embedding),
+      ]
+    );
 
-  res.json(newTodo.rows[0]);
+    res.json(newTodo.rows[0]);
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 // update tasks for tenant - note that we don't handle partial updates here
@@ -127,7 +87,6 @@ app.post("/api/tenants/:tenantId/todos", async (req, res) => {
 app.put("/api/tenants/:tenantId/todos", async (req, res) => {
   try {
     const { id, complete } = req.body;
-    nile.tenantId = req.params.tenantId;
     await nile.db.query(
       `UPDATE todos 
        SET complete = $1
@@ -174,6 +133,6 @@ app.get("/insecure/all_todos", async (req, res) => {
 });
 
 // All other GET requests not handled before will return the React app's index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../fe', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../fe", "index.html"));
 });
