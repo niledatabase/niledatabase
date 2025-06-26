4c3f8156bfe1ebbe06b670460a3d153560aaba81
jrea <juzko.r@gmail.com>
2025-06-26 12:00:10 -0400

    chore(examples): node_react to v5

--- examples/quickstart/node_react/src/index.mjs
+++
@@ -1,6 +0,0 @@
-// "Create React App" tooling expects to find the WWW root here,
-// so we simply use this file to import the ./www directory.
-
-import * as fe from "./fe/index.mjs";
-
-export default fe;
